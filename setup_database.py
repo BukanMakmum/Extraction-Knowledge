3bc6a3e99d9ae57fe58a91b8c5dba659e22a388f
Imam <32379649+BukanMakmum@users.noreply.github.com>
2023-06-02 03:44:04 +0700

    Update setup_database.py
    Signed-off-by: Imam <32379649+BukanMakmum@users.noreply.github.com>

--- setup_database.py
+++ setup_database.py
@@ -1,17 +1,39 @@
 import mysql.connector
 
+# Mengatur koneksi ke database MySQL
+db = mysql.connector.connect(
+    host="localhost",
+    user="imam",
+    password="12345"
+)
+
+# Membuat database jika belum ada
+
+
+def create_database():
+    cursor = db.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS uas_km")
+    print("Database created successfully!")
+
+# Membuat tabel untuk menyimpan pengetahuan eksplisit
+
+
 def create_table():
-    try:
-        conn = mysql.connector.connect(
-            host="localhost",
-            user="imam",
-            password="12345",
-            database="project_uas"
-        )
-        c = conn.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS extracted_knowledge (category VARCHAR(255), sentence TEXT, accuracy FLOAT)''')
-        conn.commit()
-        conn.close()
-        print("Table created successfully!")
-    except mysql.connector.Error as error:
-        print("Error creating table:", error)
+    cursor = db.cursor()
+    cursor.execute("USE uas_km")
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS extracted_knowledge (id INT AUTO_INCREMENT PRIMARY KEY, category VARCHAR(255), sentence TEXT, accuracy FLOAT)")
+    print("Table created successfully!")
+
+# Menutup koneksi database
+
+
+def close_connection():
+    db.close()
+
+
+# Menjalankan fungsi setup
+if __name__ == "__main__":
+    create_database()
+    create_table()
+    close_connection()
